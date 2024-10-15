hacks-guide/Guide_Wii

config: add Discord invite to navbar

docs/.vitepress/config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
   },
   themeConfig: {
     socialLinks: [
+      { icon: 'discord', link: 'https://discord.gg/MWxPgEp' },
       { icon: 'github', link: 'https://github.com/hacks-guide/Guide_Wii' }
     ]
   },

nh-server/switch-guide

docs/.vitepress/config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
   head: [['link', { rel: 'icon', href: '/img/favicon.ico' }]],
   themeConfig: {
     socialLinks: [
+      { icon: 'discord', link: 'https://discord.gg/C29hYvh' },
       { icon: 'github', link: 'https://github.com/nh-server/switch-guide' }
     ]
   },